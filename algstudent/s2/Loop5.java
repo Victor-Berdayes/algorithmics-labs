89d0b834b5f095965393cf24b8cb44b26705cbd9
Víctor Berdayes García-Pumarino <UO300218@uniovi.es>
2025-02-17 19:55:41 +0100

    Added the files of session2

---
+++ algstudent/s2/Loop5.java
@@ -0,0 +1,36 @@
+package algstudents.s12;
+
+public class Loop5 {
+	
+	public static long loop5(int n) {
+		long cont = 0;
+		for (int i = 1; i <= n; i++) //n
+			for (int j = 1; j <= n; j++) //n 
+				for (int k = 1; k <= n; k*=3) // log(n)
+					for(int l = 1; l <=n; l*=2) // log(n)
+						cont++;
+		//n^2 log(n)^2
+		return cont;
+
+	}
+
+	public static void main(String arg[]) {
+		long c = 0;
+		long t1, t2;
+
+		int nTimes = Integer.parseInt(arg[0]);
+
+		System.out.println("n\ttime\trepetions\tcounter");
+
+		for (int n = 100; n <= 819200; n *= 2) {
+			t1 = System.currentTimeMillis();
+
+			for (int repetitions = 1; repetitions <= nTimes; repetitions++)
+				c = loop5(n);
+
+			t2 = System.currentTimeMillis();
+
+			System.out.println(n + "\t" + (t2 - t1) + "\t" + nTimes + "\t\t" + c);
+		} // for
+	} // main
+}
